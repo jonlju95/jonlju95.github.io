53b3f897a78ba86bd4fc8ed0911a3f966204683c
Jonatan Ljung <jonatan.ljung@hotmail.com>
2024-07-15 18:21:28 +0200

    Navbar progress

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarService } from '../../../services/sidebarService';
+import { ThemeService } from '../../../services/themeService';
+import { LanguageService } from '../../../services/languageService';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(public sidebarService: SidebarService) {}
+  constructor(public themeService: ThemeService, public languageService: LanguageService) { }
 
-  public toggleSidebar() {
-    // this.sidebarService.toggleSidebar();
+  public onThemeSwitchChange() {
+    this.themeService.toggleTheme();
   }
 
+  public onLanguageChange() {
+    const currentLang = this.languageService.getCurrentLang() === 'en' ? 'se' : 'en';
+    this.languageService.setLanguage(currentLang);
+  }
 }
