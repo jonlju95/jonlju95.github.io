1077f961f3b6c85c362d91a057c495cb0d50d972
Jonatan Ljung <jonatan.ljung@hotmail.com>
2024-07-25 11:45:52 +0200

    Remade structure. Source code is now in separate project

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-BSPW5AY5.mjs";import"./chunk-X6XOISGT.mjs";import"./chunk-FPNM7SF3.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/common/home/home.component';
-import { AboutComponent } from './components/common/about/about.component';
-
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent, data: { animation: 'home' } },
-  { path: 'about', component: AboutComponent, data: { animation: 'about' } },
-];
